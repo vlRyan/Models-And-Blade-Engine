170ab47c63acfacb6b9dfdf40457166eacfa6360
Ryan <ryan.pulocan6@gmail.com>
2024-04-14 19:24:34 +0800

    Create Book Model

---
+++ app/Models/Models/Book.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    use HasFactory;
+}
